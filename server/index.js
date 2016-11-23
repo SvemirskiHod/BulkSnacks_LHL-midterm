79361c44439773c011864573c1648d97fe930501
Micah Britton <micahwkb@gmail.com>
2016-11-23 10:08:49 -0500

    minor additional server pre-config

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 'use strict';
 
-const express = require('express');
-const session = require('cookie-session');
-const fs = require('fs');
-
+const pg       = require('knex')(knex_dev);
+const fs       = require('fs');
+const express  = require('express');
+const session  = require('cookie-session');
 const knex_dev = require('./knexfile').development;
-const pg = require('knex')(knex_dev);
+
+const PORT = 8080;
+const app  = require('express');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 pg.destroy();
