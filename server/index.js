3a35178ef2213ac48a49c8b432ccd6e7fb0f800f
Micah Britton <micahwkb@gmail.com>
2016-11-23 13:29:53 -0500

    fixed improper declaration of 'app' in index.js, moved const declarations around for more natural readability, knexfile modified to use static db info per compass'

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const pg       = require('knex')(knex_dev);
-const fs       = require('fs');
-const express  = require('express');
-const session  = require('cookie-session');
-const knex_dev = require('./knexfile').development;
+const knex_dev   = require('./knexfile').development;
+const pg         = require('knex')(knex_dev);
+const fs         = require('fs');
+const express    = require('express');
+const session    = require('cookie-session');
+const bodyParser = require('body-parser');
 
 const PORT = 8080;
-const app  = require('express');
+const app  = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-pg.destroy();
--- server/knexfile.js
+++ server/knexfile.js
@@ -5,12 +5,12 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      user     : settings.user,
-      password : settings.password,
-      database : settings.database,
-      host     : settings.hostname,
-      port     : settings.port,
-      ssl      : settings.ssl
+      user     : 'labber',
+      password : 'labber',
+      database : 'midterm',
+      host     : 'localhost',
+      port     : 5432,
+      ssl      : true,
     },
     debug: true
   },
