febe6397d0b9ffddaba37ef9dadcdb204a8da8cc
Micah Britton <micahwkb@gmail.com>
2016-11-23 15:36:39 -0500

    built initial migration and seed data, tested, snacks db populating as hoped

--- db/seeds/initial_seed.js
+++ db/seeds/initial_seed.js
@@ -5,19 +5,62 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('snacks').insert([
-
-        ])
+        knex('snacks').insert(
+          [
+            {
+              'name': 'Party Mix',
+              'price-per-100g': 180,
+              'img-filename': 'party-mix.png'
+            },
+            {
+              'name': 'Animal Crackers',
+              'price-per-100g': 125,
+              'img-filename': 'animal-crackers.png'
+            },
+            {
+              'name': 'Bazinga Mix',
+              'price-per-100g': 220,
+              'img-filename': 'bazinga.png'
+            },
+            {
+              'name': 'Bits n Bites Mix',
+              'price-per-100g': 220,
+              'img-filename': 'bits-n-bites.png'
+            },
+            {
+              'name': 'Milk Chocolate-covered Pretzels',
+              'price-per-100g': 200,
+              'img-filename': 'choco-pretz.png'
+            },
+            {
+              'name': 'Chocolicious Party Mix',
+              'price-per-100g': 250,
+              'img-filename': 'chocolicious.png'
+            },
+            {
+              'name': 'Dried Veggy Chips',
+              'price-per-100g': 175,
+              'img-filename': 'dried-veggy-chips.png'
+            },
+            {
+              'name': 'Hippie Trail Mix',
+              'price-per-100g': 180,
+              'img-filename': 'hippie-mix.png'
+            },
+            {
+              'name': 'M&Ms Peanuts',
+              'price-per-100g': 240,
+              'img-filename': 'm-m-peanut.png'
+            },
+            {
+              'name': 'Mini Chocolate Chip Cookies',
+              'price-per-100g': 125,
+              'img-filename': 'mini-choc-chip-cookies.png'
+            }
+          ])
       ]);
     });
 };
 
 
 
-[
-  {
-    name: 'Party Mix',
-    price-per-100g: ''
-    img-filename:
-  }
-]
--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,3 @@
-const settings = require("./settings");
-
 module.exports = {
 
   development: {
@@ -8,7 +6,7 @@ module.exports = {
       host     : process.env.DB_HOST,
       user     : process.env.DB_USER,
       password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
+      database : 'midterm',
       port     : process.env.DB_PORT,
       ssl      : process.env.DB_SSL
     },
