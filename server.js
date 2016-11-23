aeee68e0161fb7dd9e76480076c54409dbc09e16
Micah Britton <micahwkb@gmail.com>
2016-11-23 18:28:58 -0500

    created initial site layout based on jumbotron, style.scss is now the main render file for node-sass (replacing problematic node-sass-middleware)

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
+const sass        = require("node-sass");
 const app         = express();
 
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
+const path        = require('path');
+const fs          = require("fs");
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
@@ -27,13 +29,32 @@ app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
+/*app.use(sass({
+  src: path.join(__dirname + "/styles"),
+  dest: path.join(__dirname + "/public/styles"),
   debug: true,
-  outputStyle: 'expanded'
-}));
+  outputStyle: 'compressed'
+}));*/
+sass.render({
+  file: "./styles/style.scss",
+  outputStyle: "compressed",
+}, (err, data) => {
+  if(!err) {
+    fs.writeFile("./public//styles/style.css", data.css, (err) => {
+      if(!err) {
+        console.log("Successfully compiled SCSS");
+      } else console.log(err);
+    });
+  } else console.log(err);
+});
+
 app.use(express.static("public"));
+// redirect CSS bootstrap
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+// redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+// redirect JS jQuery
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
