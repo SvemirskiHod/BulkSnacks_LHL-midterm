0574c788751eb0498a200c9f3e3fef70b418869b
Micah Britton <micahwkb@gmail.com>
2016-11-24 14:53:45 -0500

    built partials for _head and _scripts, tested against index.ejs (looks OK)

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,31 @@
 "use strict";
 
-const express = require('express');
-const router  = express.Router();
+const express     = require('express');
+const router      = express.Router();
+const dataHelpers = require('../server/data-helpers');
 
-const checkPassword = (loginData) => {
-  console.log(loginData[0].password);
-};
+// const checkPassword = (loginData) => {
+//   console.log(loginData[0].password);
+// };
 
 module.exports = (knex) => {
 
   router.post("/login", (req, res) => {
     const email    = req.body.email;
+    const password = req.body.password;
     knex("accounts")
       .select("*")
       .where('email', email)
-      .then(checkPassword)
+      .then((result) => {
+        // console.log("knex: ", password, result)
+        dataHelpers.checkPassword(result, password);
+      })
       .catch(function(error) {
         console.error(error);
       });
   });
-
+  // example below - we can add more routes to the exports
+  // ** keep it related to 'users' for this module! **
   router.get("/test", (req, res) => {
     res.send("It works!");
   });
