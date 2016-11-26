ed8c45ead6586da596a6954878fc4b164e0b1614
Your name <jaocoelho93@gmail.com>
2016-11-26 21:13:16 +0000

    Made basketscripts more DRY

--- public/scripts/basketscripts.js
+++ public/scripts/basketscripts.js
@@ -8,45 +8,43 @@ var total = 0;
 }
 
 var updateQuantity = function(input, currentVal, $min, n) {
-  if(currentVal != $min){
-    input.val(Number(currentVal) + n);
+  var itemCount = Number(currentVal)
+
+  if (((input.val() > 0) && n === 1) || ((input.val() > 1) && n === -1)) {
+    itemCount += n;
+    input.val(itemCount);
   }
+
+  currentVal = itemCount;
   var id = Number((input.attr("id")));
   basket = JSON.parse(localStorage.getItem('basket'));
   basket[id] = currentVal;
   localStorage.setItem('basket', JSON.stringify(basket));
 }
 
-var buttonWatcher = function(){
- $(".reduce-quantity, .add-quantity").on("click", function(event){
-  var input = $(this).siblings("input");
-  var currentVal = input.val();
-  var $min = input.attr("min")
-  if ($(this).attr('class') === "reduce-quantity"){
-    var n = -1;
-  }
-  else {
-    var n = 1;
-
-  }
-  updateQuantity(input, currentVal, $min, n);
-
-  var quantity = Number(input.val());
-  var price = Number($(this).parent().siblings().find('.price').text())
-  var productTotal = quantity*price;
-  $(this).parent().siblings().find('.product-total').text(productTotal.toFixed(2));
-
-  updateTotal();
-  });
-
-}
-
-
 
 $(document).ready(function() {
 
   updateTotal();
-  buttonWatcher();
 
+  $(".reduce-quantity, .add-quantity").on("click", function(event){
+    var input = $(this).siblings("input");
+    var currentVal = input.val();
+    var $min = input.attr("min")
+    if ($(this).attr('class') === "reduce-quantity"){
+      var n = -1;
+    }
+    else {
+     var n = 1;
+    }
+    updateQuantity(input, currentVal, $min, n);
+
+    var quantity = Number(input.val());
+    var price = Number($(this).parent().siblings().find('.price').text())
+    var productTotal = quantity*price;
+    $(this).parent().siblings().find('.product-total').text(productTotal.toFixed(2));
+
+    updateTotal();
+  });
 });
 
